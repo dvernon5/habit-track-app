4259d4ceaed0aa8baadba42ddb0f69ed312295ad
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2025-05-29 21:44:25 -0700

    Update db.js
    Notifying user to check out the .env.example file to set up and connect their MongoDB Database account.

--- server/config/db.js
+++ server/config/db.js
@@ -5,6 +5,7 @@ require("dotenv").config(); // Load environment variables from .env file.
 // Connect to MongoDB using Mongoose.
 const connectDB = async () => {
     try {
+        // Check out .env.example file to connect your Database.
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB connected successfully");
     } catch (error) {
