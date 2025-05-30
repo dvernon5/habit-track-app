c29bc2a17e382544371b1713f490b4b481cd612b
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2025-05-29 21:45:42 -0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,12 +5,12 @@ require("dotenv").config(); // Load environment variables from .env file.
 // Connect to MongoDB using Mongoose.
 const connectDB = async () => {
     try {
-        // Check out .env.example file to connect your Database.
+        // Check out .env.example file to connect your MongoDB database.
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection error:", error);
-        process.exit(1); // Exit the process wiith failure.
+        process.exit(1);
     }
 }
 
